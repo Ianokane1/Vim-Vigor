3c6391be19d6e078de66f3d2e5c597f836e3ea4a
Ian Kane <ianwilliamkane@gmail.com>
2022-08-18 15:57:40 +0000

    Add 404 page

--- vim_vigor/urls.py
+++ vim_vigor/urls.py
@@ -16,7 +16,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +28,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-handler404 = 'home.views.error_404'
+handler404 = 'vim_vigor.views.hander404'
