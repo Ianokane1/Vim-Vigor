ca351b7fccf6f1e76c9a2e3a4b5403e0010ed72b
Ian Kane <ianwilliamkane@gmail.com>
2022-08-18 20:34:34 +0000

    Added footer for newsletter link, blog link and social media links

--- vim_vigor/settings.py
+++ vim_vigor/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vim-vigor.herokuapp.com', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-ianokane1-vimvigor-6r1xq3h1uho.ws-eu62.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-ianokane1-vimvigor-6r1xq3h1uho.ws-eu62.gitpod.io, vim-vigor.herokuapp.com']
 
 
 # Application definition
