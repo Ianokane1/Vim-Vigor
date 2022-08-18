df1ff823fc760dd3996f539d92284d47ae36d751
Ian Kane <ianwilliamkane@gmail.com>
2022-08-18 19:05:39 +0000

    Added env

---
+++ env.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIARF5WK76E5IOHN44P"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
+os.environ["DATABASE_URL"] = "postgres://amatnjoxwilgcm:be682840ef3a3d5c22c668a695f0e3158f49c0e3816796350775dc487d349202@ec2-34-253-119-24.eu-west-1.compute.amazonaws.com:5432/d5e7vus1kgl2d3"
+os.environ["EMAIL_HOST_PASS"] = "fujogcimpxangmfj"
+os.environ["EMAIL_HOST_USER"] = " ianwilliamkane@gmail.com"
+os.environ["SECRET_KEY"] = "51v-4j6b(ekr*la=k23j&vpc2-wf7)eh91w5ggubojs3l5&8v^"
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51LXlEdDWkCKwb7oxFvmcNMsg3hH3pgh00wGeJFtByXSQtFo0lWkVcVGB1Zp46QoxjR2bylQ9YEzRlD7GUALjqyLP00KUATJOCS"
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51LXlEdDWkCKwb7ox2hpJSLOTtHLv86O38GJRNFzjAjes4nQMJWcco84TbU31mxGBtZGcw1zG2qYO3XfMyB5Tdpu400tuev4Gu4"
+os.environ["STRIPE_WH_SECRET"] = "whsec_MqZfKgHjP6pNtkdTdpUiROwEE0EysCZn"
+os.environ["USE_AWS"] = "True"
